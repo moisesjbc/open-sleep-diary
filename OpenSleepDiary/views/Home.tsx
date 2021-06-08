5e5636958fe458aaaa9e67dd3eb1fd6d316f6757
Moisés J <moisesjbc@gmail.com>
2021-06-08 20:25:46 +0100

    Add buttons for last seven days to Home

--- OpenSleepDiary/views/Home.tsx
+++ OpenSleepDiary/views/Home.tsx
@@ -1,14 +1,33 @@
-import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  button: {
+    marginTop: 30,
+    backgroundColor: '#ccffff'
+  }
+});
+
 
 export default function Home({ navigation }) {
+  const previousWeek = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate());
+  previousWeek.setDate(previousWeek.getDate() - 6);
+  const dates = [];
+  for (let i=0; i<7; i++) {
+    dates.push(new Date(previousWeek));
+    previousWeek.setDate(previousWeek.getDate() + 1)
+  }
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Sleep entry', {date: new Date(2021, 5, 8).toISOString()})}
-      />
+      {(dates.map((date, index) => (
+        <TouchableOpacity
+          key={index}
+          style={styles.button}
+          onPress={() => navigation.navigate('Sleep entry', {date: date.toISOString()})}>
+            <Text>{`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`}</Text>
+        </TouchableOpacity>
+      )))}
     </View>
   );
 }
